6108c82759db9ee9fdb379ab50ddac16f83726da
Ben Welsh <ben.welsh@gmail.com>
2018-12-11 22:38:25 -0800

    Update unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cat llcs.tar.bz2.part* > llcs.tar.bz2
-#cat corporations.tar.bz2.part* > corporations.tar.bz2
-#tar xvjf llcs.tar.bz2
+cat llcs.tar.bz2.part* > llcs.tar.bz2
+cat corporations.tar.bz2.part* > corporations.tar.bz2
+tar xvjf llcs.tar.bz2
 tar xvjf corporations.tar.bz2
